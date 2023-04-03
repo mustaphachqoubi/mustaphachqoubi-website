db8516bd8c76fcdb2188f68585f24d98421c04a1
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-04-03 18:40:18 +0000

    i succed moving the folder svg to cursor position but it not perfect, i have to make it perfect

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -7,36 +7,97 @@ export const Pictures = () => {
   const [folderPosition, setFolderPosition] = useState('')
   const [isMouseDown, setIsMouseDown] = useState(false)
 
-  
+//   
+// const handleMove = (m) => {
+//   setFolderPosition(`absolute top-${m.clientY} left-[${m.clientX}]`)
+//   console.log(m.clientX, m.clientY)
+// }
+//
+// useEffect(() => {
+//   const closedfolder = document.getElementById('closedfolder')
+//   const handleMouseDown = () => {
+//     window.addEventListener('mousemove', handleMove);
+//     setIsMouseDown(true);
+//   };
+//
+//   const handleMouseUp = () => {
+//     window.removeEventListener('mousemove', handleMove);
+//     setIsMouseDown(false)
+//   };
+//
+//   window.addEventListener('mousedown', handleMouseDown);
+//   window.addEventListener('mouseup', handleMouseUp);
+//
+//   return () => {
+//     window.removeEventListener('mousedown', handleMouseDown);
+//     window.removeEventListener('mouseup', handleMouseUp);
+//     window.removeEventListener('mousemove', handleMove);
+//   };
+// }, [setFolderPosition]);
+
+
+// const handleMove = (m) => {
+//   const folder = document.getElementById('folder')
+//
+//   let x = m.pageX;
+//   let y = m.pageY;
+//
+//   folder.style.position = 'absolute'
+//   folder.style.left = x - 10 + 'px';
+//   folder.style.top = y - 10 + 'px';
+// }
+//
+// const handlePosition = () => {
+//   window.addEventListener('mousemove', handleMove)
+//   window.addEventListener('mouseup', handlePreviousPosition)
+// }
+//
+// const handlePreviousPosition = () => {
+//   const folder = document.getElementById('folder')
+//   folder.style.position = 'static'
+//
+//   window.removeEventListener('mousemove', handleMove)
+//   window.removeEventListener('mouseup', handlePreviousPosition)
+// }
+
+
+
+
 const handleMove = (m) => {
-  setFolderPosition(`absolute top-${m.clientY} left-[${m.clientX}]`)
-  console.log(m.clientX, m.clientY)
-}
+  const folder = document.getElementById('folder');
+  const rect = folder.getBoundingClientRect();
 
-useEffect(() => {
-  const closedfolder = document.getElementById('closedfolder')
-  const handleMouseDown = () => {
-    window.addEventListener('mousemove', handleMove);
-    setIsMouseDown(true);
-  };
+  let x = m.pageX - rect.width / 2;
+  let y = m.pageY - rect.height / 2;
 
-  const handleMouseUp = () => {
-    window.removeEventListener('mousemove', handleMove);
-    setIsMouseDown(false)
-  };
+  requestAnimationFrame(() => {
+    folder.style.position = 'absolute';
+    folder.style.left = x + 'px';
+    folder.style.top = y + 'px';
+  });
+};
 
-  window.addEventListener('mousedown', handleMouseDown);
-  window.addEventListener('mouseup', handleMouseUp);
+const handlePosition = () => {
+  window.addEventListener('mousemove', handleMove);
+  window.addEventListener('mouseup', handlePreviousPosition);
+};
 
-  return () => {
-    window.removeEventListener('mousedown', handleMouseDown);
-    window.removeEventListener('mouseup', handleMouseUp);
-    window.removeEventListener('mousemove', handleMove);
-  };
-}, [setFolderPosition]);
+const handlePreviousPosition = () => {
+  const folder = document.getElementById('folder');
+  folder.style.position = 'static';
+
+  window.removeEventListener('mousemove', handleMove);
+  window.removeEventListener('mouseup', handlePreviousPosition);
+};
+
+document.addEventListener('mouseup', () => {
+  const folder = document.getElementById('folder');
+  folder.style.position = 'static';
+
+})
 
 
-  return (
+return (
     <>
       <div
       className="flex flex-col gap-5 items-center cursor-pointer ">
@@ -44,10 +105,17 @@ useEffect(() => {
           isClicked === false ? setIsClicked(true) : setIsClicked(false)
         }}>
         {
-            isClicked ? (<div className={`${folderPosition} cursor-pointer active:scale-100 hover:scale-110 duration-300`}><OpenedFolder /></div>) : (
+            isClicked ? (
             <div
-            id="closedfolder"
-            className={`${folderPosition} cursor-pointer active:scale-100 hover:scale-110 duration-300`}><ClosedFolder /></div>)
+            id="folder"
+            onMouseDown={handlePosition}  
+            onMouseUp={handlePreviousPosition}
+            className={`folder cursor-pointer active:scale-100 hover:scale-110 duration-300`}><OpenedFolder /></div>) : (
+            <div
+            id="folder"
+            onMouseDown={handlePosition}
+            onMouseUp={handlePreviousPosition}
+            className={`folder cursor-pointer active:scale-100 hover:scale-110 duration-300`}><ClosedFolder /></div>)
           }
 
         </div>
