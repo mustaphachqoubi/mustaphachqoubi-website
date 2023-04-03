8b420208554e05e488df0a4212eed33d6170906c
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-04-03 01:28:07 +0000

    i couldn't make the drag folder feature for now

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -4,25 +4,55 @@ import { OpenedFolder } from '../assets/OpenedFolder.jsx'
 
 export const Pictures = () => {
   const [isClicked, setIsClicked] = useState(false);
-  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [folderPosition, setFolderPosition] = useState('')
+  const [isMouseDown, setIsMouseDown] = useState(false)
 
-  useEffect(() => {
-    // isMouseDown ? console.log(isMouseDown) : console.log(false)
-  }, [isMouseDown])
+  
+const handleMove = (m) => {
+  setFolderPosition(`absolute top-${m.clientY} left-[${m.clientX}]`)
+  console.log(m.clientX, m.clientY)
+}
+
+useEffect(() => {
+  const closedfolder = document.getElementById('closedfolder')
+  const handleMouseDown = () => {
+    window.addEventListener('mousemove', handleMove);
+    setIsMouseDown(true);
+  };
+
+  const handleMouseUp = () => {
+    window.removeEventListener('mousemove', handleMove);
+    setIsMouseDown(false)
+  };
+
+  window.addEventListener('mousedown', handleMouseDown);
+  window.addEventListener('mouseup', handleMouseUp);
+
+  return () => {
+    window.removeEventListener('mousedown', handleMouseDown);
+    window.removeEventListener('mouseup', handleMouseUp);
+    window.removeEventListener('mousemove', handleMove);
+  };
+}, [setFolderPosition]);
+
 
   return (
     <>
       <div
-      className="flex flex-col gap-5 items-center cursor-pointer">
+      className="flex flex-col gap-5 items-center cursor-pointer ">
         <div onClick={() => {
           isClicked === false ? setIsClicked(true) : setIsClicked(false)
         }}>
-          {
-            isClicked ? <OpenedFolder /> : <ClosedFolder />
+        {
+            isClicked ? (<div className={`${folderPosition} cursor-pointer active:scale-100 hover:scale-110 duration-300`}><OpenedFolder /></div>) : (
+            <div
+            id="closedfolder"
+            className={`${folderPosition} cursor-pointer active:scale-100 hover:scale-110 duration-300`}><ClosedFolder /></div>)
           }
+
         </div>
         <div>
-          <h1 className="font-bold text-sm tracking-wide">
+          <h1 className="font-bold text-sm tracking-wide select-none ">
             Some Pictures of me
           </h1>
         </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,16 +6,16 @@ export const Work = () => {
 const [isClicked, setIsClicked] = useState(false);
   return (
     <>
-      <div className="flex flex-col items-center gap-5 cursor-pointer">
+      <div className="flex flex-col items-center gap-5 ">
         <div onClick={() => {
           isClicked === false ? setIsClicked(true) : setIsClicked(false)
         }}>
           {
-            isClicked ? <OpenedFolder /> : <ClosedFolder />
+            isClicked ? (<div className="cursor-pointer hover:scale-110 duration-300"><OpenedFolder /></div>) : (<div className="cursor-pointer hover:scale-110 duration-300"><ClosedFolder /></div>)
           }
         </div>
         <div>
-          <h1 className="font-bold text-sm tracking-wide">
+          <h1 className="font-bold text-sm tracking-wide select-none ">
           Some Examples of my work
           </h1>
         </div>
