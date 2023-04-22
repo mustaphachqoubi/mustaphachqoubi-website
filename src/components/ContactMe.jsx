c1778794b5a599769e8171d0a7980bb2c0437bac
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-04-22 06:25:52 +0000

    i am trying to add the inkdrop effect so i succeed at tracking the pen position and make the drop place at the same level with it

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,13 +1,29 @@
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
 import {InkPen} from '../assets/InkPen.jsx'
 import {CartoonGuy} from '../assets/CartoonGuy.jsx'
+import { useState, useEffect} from 'react'
 
 export const ContactMe = ({bg}) => {
+  window.addEventListener('resize', () => {
+    const waves = document.getElementById('waves')
+    const inkpen = document.getElementById('inkpen')
+
+    waves.style.position = 'absolute';
+    waves.style.left = inkpen.offsetLeft + 'px'
+  })
+  useEffect(() => {
+    const waves = document.getElementById('waves')
+    const inkpen = document.getElementById('inkpen')
+
+    waves.style.position = 'absolute';
+    waves.style.left = inkpen.offsetLeft + 'px'
+  }, [])
+
   return (
     <div id="contactme" className={`relative h-screen flex flex-col justify-center gap-20 items-center ${bg} mix-blend-difference`}>
       
-      <div className="w-full flex justify-end absolute top-0">
-        <div className='w-60'>
+      <div className="w-full flex justify-end absolute top-0 ">
+        <div id="inkpen" className='w-60'>
         <InkPen />
         </div>
       </div>
@@ -17,7 +33,7 @@ export const ContactMe = ({bg}) => {
         <h3 className="text-xs lg:text-md underline underline-offset-2 decoration-2">Contact me if you want more.</h3>
       </div>
 
-      <div className="flex gap-5 justify-center items-center text-2xl xl:text-3xl">
+      <div className="flex gap-5 justify-center items-center text-2xl xl:text-3xl ">
         <a href="https://www.linkedin.com/in/mustapha-chqoubi/" target="_blank" rel="noreferrer" className="cursor-pointer">
           <BsLinkedin />
         </a>
@@ -26,7 +42,10 @@ export const ContactMe = ({bg}) => {
         </a>
       </div>
 
-      <div className="w-full flex justify-center absolute bottom-0">
+      <div className="w-full flex justify-center absolute bottom-0 bg-red-500">
+        <div id='waves' className="bg-green-500 w-20 h-8">
+          
+        </div>
         <div className='w-[10rem] sm:w-[13rem]'>
         <CartoonGuy />
         </div>
