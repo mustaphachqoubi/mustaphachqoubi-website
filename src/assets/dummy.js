3e367249a9a7ff0ae657bb9d1666d345b2f1ef2e
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-05-03 16:35:49 +0100

    i change the michanism of services display

--- src/assets/dummy.js
+++ src/assets/dummy.js
@@ -1,42 +1,160 @@
-export const services = [
-    {
-      id: 1,
-      servicesTitle: "Landing Pages",
-      services: [
-        {
-          id: 1,
-          name: "Simple landing page",
-          price: 500,
-          deadline: 10,
-        fullFeatures: false,
-          features: [
-          { id: 1, name: "Minimalistic design" },
-        ]
-        },
-        { id: 2, name: "Complex landing page", price: 1000, deadline: 20, fullFeatures: false, features: [
-          { id: 1, name: "Unique design" },
-          { id: 2, name: "3D graphics" },
-        ]
-},
-        { id: 3, name: "Unachievable", price: ' +3000', deadline: 30, fullFeatures: true},
-      ],
-    },
-    {
-      id: 2,
-      servicesTitle: "Portfolio Websites",
-      services: [
-        { id: 1, name: "Portfolio One", price: 899, pricePhour: 5 },
-        { id: 2, name: "Portfolio two", price: 999, pricePhour: 6 },
-        { id: 3, name: "portfolio three", price: 1999, pricePhour: 7 },
-      ],
-    },
-  ];
 
-export  const features = [
-    { id: 1, name: "Minimalistic design" },
-    { id: 2, name: "Unique design" },
-    { id: 3, name: "3D graphics" },
-    { id: 4, name: "Animations" },
-    { id: 5, name: "test" },
-  ];
+export const services = [
+  {
+    id: 1,
+    title: "Landing Page Website",
+    items: [
+      {
+        id: 1,
+        title: "Basic Landing Page",
+        features: [
+          "Custom design based on your brand",
+          "Mobile-responsive layout",
+          "Lead capture form",
+          "Basic SEO optimization"
+        ],
+        price: "$500",
+      },
+      {
+        id: 2,
+        title: "Advanced Landing Page",
+        features: [
+          "Custom design with interactive elements",
+          "Advanced lead capture forms",
+          "Advanced SEO optimization",
+          "Integration with third-party tools"
+        ],
+        price: "$1,500"
+      },
+      {
+        id: 3,
+        title: "Custom Landing Page",
+        features: [
+          "Unique design that aligns with your business goals",
+          "Custom user experience",
+          "Comprehensive website strategy and planning",
+          "Ongoing website optimization and support"
+        ],
+        price: "$3,000"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Website Maintenance",
+    items: [
+      {
+        id: 1,
+        title: "Basic Maintenance Package",
+        features: [
+          "Weekly backups to protect your website",
+          "Software updates",
+          "Basic security monitoring",
+          "Priority support"
+        ],
+        price: "$300/month"
+      },
+      {
+        id: 2,
+        title: "Advanced Maintenance Package",
+        features: [
+          "Daily backups",
+          "Advanced security monitoring",
+          "Website speed optimization",
+          "Monthly website analytics reports"
+        ],
+        price: "$500/month"
+      },
+      {
+        id: 3,
+        title: "Custom Maintenance Package",
+        features: [
+          "Customized maintenance plan",
+          "Priority support with guaranteed response times",
+          "On-demand website updates and fixes",
+          "Website hosting and management services"
+        ],
+        price: "$1,000/month"
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Search Engine Optimization",
+    items: [
+      {
+        id: 1,
+        title: "Basic SEO Package",
+        features: [
+          "Keyword research and analysis",
+          "On-page optimization",
+          "Local SEO optimization",
+          "Monthly performance reports"
+        ],
+        price: "$500/month"
+      },
+      {
+        id: 2,
+        title: "Advanced SEO Package",
+        features: [
+          "In-depth SEO audit",
+          "Advanced keyword research and analysis",
+          "Technical SEO optimization",
+          "Link building and content marketing strategies"
+        ],
+        price: "$1,000/month"
+      },
+      {
+        id: 3,
+        title: "Custom SEO Package",
+        features: [
+          "Customized SEO plan tailored to your business goals",
+          "Advanced analytics and reporting",
+          "Comprehensive competitive analysis",
+          "Personalized SEO coaching and support"
+        ],
+        price: "$2,000/month"
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: "Website Design & Development",
+    items: [
+      {
+        id: 1,
+        title: "Basic Website Design",
+        features: [
+          "Customized design that matches your brand",
+          "Mobile-friendly design",
+          "Easy-to-use content management system",
+          "Quick turnaround time"
+        ],
+        price: "$1,500"
+      },
+      {
+        id: 2,
+        title: "Advanced Website Design",
+        features: [
+          "Customized design with advanced functionality",
+          "Optimized user experience for better engagement",
+          "Advanced content management system",
+          "Integration with third-party tools"
+        ],
+        price: "Starting at $3,000"
+      },
+      {
+        id: 3,
+        title: "Custom Website Design",
+        features: [
+          "Unique design tailored to your business goals",
+          "Custom user experience",
+          "Comprehensive website strategy and planning",
+          "Ongoing website optimization and support"
+        ],
+        price: "Starting at $5,000"
+      }
+    ]
+  }
+];
 
