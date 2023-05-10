4591478ee7fb1df225cf18b8da7048c6c58a64a7
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-05-10 17:00:35 +0100

    i added the readme file and i am working on the single service page right now

---
+++ README.md
@@ -0,0 +1,33 @@
+# Project Folder Structure
+`
+.
+├── README.md
+├── backend
+│   ├── controllers
+│   ├── models
+│   ├── node_modules
+│   ├── package.json
+│   ├── pnpm-lock.yaml
+│   ├── routes
+│   └── server.js
+└── frontend
+    ├── README.md
+    ├── node_modules
+    ├── package.json
+    ├── pnpm-lock.yaml
+    ├── public
+    ├── src
+    └── tailwind.config.js
+`
+
+# Project Stack
+`React.js`: A JavaScript library for building user interfaces.
+`Tailwind CSS`: A utility-first CSS framework for quickly building custom user interfaces.
+`CSS`: Cascading Style Sheets, a language used to style HTML content.
+`Redux.js`: A state management library for JavaScript applications.
+`GSAP`: GreenSock Animation Platform, a JavaScript library for creating high-performance animations.
+`React Router`: A library for declaratively routing in React applications.
+`React Sound`: A React component for playing sound effects and music.
+`Express`: A minimalist web framework for Node.js for building APIs and web applications.
+`Node.js`: A JavaScript runtime for building scalable network applications.
+`MongoDB`: A NoSQL document-oriented database used to store and retrieve data in a flexible, JSON-like format.
--- frontend/src/components/Service.jsx
+++ frontend/src/components/Service.jsx
@@ -1,24 +1,33 @@
 import { useParams } from 'react-router-dom'
 import { services } from '../assets/dummy.js'
 import { useEffect, useState } from 'react'
+import { FaGreaterThan } from 'react-icons/fa'
+import { AiOutlineCaretDown, AiFillCaretRight } from 'react-icons/ai'
+import starBackground from '../assets/starBackground.mp4'
+import galaxyBackground from '../assets/galaxyBackground.mp4'
 
 export const Service = () => {
   const { service, itemId} = useParams();
   const [serviceItemId, setServiceItemId] = useState('')
   const [itemOfService, setItemOfService] = useState({})
   const [optionSelected, setIsOptionSelected] = useState(false)
+  const [featuresOpened, setFeaturesOpened] = useState(false)
+  const [videoSrc, setVideoSrc] = useState(starBackground)
+  const [srcChanger, setSrcChanger] = useState(1)
 
-  const getSingleItem = async (service, itemId) => {
-    const item = await itemOfService;
-    try{
-
-    }catch (err){
-
+  useEffect(() => {
+ if(videoSrc === galaxyBackground){
+      setVideoSrc(starBackground)
+      console.log("star",starBackground)
+    }else{
+      setVideoSrc(galaxyBackground)
+      console.log("galaxy",galaxyBackground)
     }
-  }
+
+  }, [srcChanger])
 
   return (
-    <div className="h-screen flex justify-center items-center p-10 pt-28">
+    <div className="h-full flex justify-center items-center p-10 py-32 ">
       {
         !itemOfService ? <div>Loading...</div> : <div className="flex flex-col w-full gap-10 items-center">
           <div className="overflow-hidden bg-white w-full sm:w-96 h-20 text-black rounded-lg flex gap-6 justify-center items-center font-bold tracking-wide">
@@ -40,17 +49,26 @@ export const Service = () => {
           </div>
 
           <div>
-            <p>{'>'} See features</p>
+            <p 
+              onMouseOver={() => {
+                featuresOpened === true ? setFeaturesOpened(false) : setFeaturesOpened(true)
+              }}
+              className="text-sm flex gap-1 justify-center items-center underline hover:no-underline cursor-pointer">{!featuresOpened ? <AiFillCaretRight /> : <AiOutlineCaretDown />} See features</p>
           </div>
 
-          <div className="flex flex-col items-center gap-16 w-full ">
+          <div className="flex flex-col items-center gap-16 w-full">
             <div className="font-bold bg-white h-20 sm:w-96 p-6 w-full rounded-lg flex flex-col gap-2 justify-center items-center text-center">
-              <p className="text-xs">You are happy with this Total ?</p>
+              <p className="text-xs">Are you happy with this Total ?</p>
               <p className="text-md">$ 1500</p>
             </div>
 
-            <div className="bg-black flex justify-center items-center w-full sm:w-96 h-20 rounded-lg text-white font-bold ">
-              <p>Let's talk</p>
+            <div 
+              onClick={() => setSrcChanger(srcChanger => srcChanger + 1)}
+            className="cursor-pointer bg-black flex justify-center overflow-hidden items-center w-full relative sm:w-96 h-20 rounded-lg text-white font-bold">
+              <video className="absolute top-0 left-0 bottom-0 right-0 " autoPlay muted loop>
+                <source src={videoSrc} type="video/mp4" />
+              </video>
+              <p className="z-20">Let's talk</p>
             </div>
           </div>
         </div>
