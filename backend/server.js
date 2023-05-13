c8f5a6a4b5ff9007daa2871617a00f4dc8f47ac7
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-05-13 20:44:14 +0100

    nothing crazy, just some testing today

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use('/api/services', services)
 
 mongoose.connect(uri)
 .then(() => {    
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Connected to MongoDb && start server on port ${port}`)
 })
 })
