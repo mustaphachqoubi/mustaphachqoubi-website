5a4e8dc226b485724d7fab231def64151376e80c
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-05-25 16:29:27 +0100

    i am working on the meeting with client component

---
+++ frontend/src/components/Meeting.jsx
@@ -0,0 +1,14 @@
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+
+export const Meeting = () => {
+
+  useEffect(()=>{
+    (async function () {
+      const cal = await getCalApi();
+      cal("ui", {"theme":"dark","styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false});
+    })();
+  }, [])
+
+  return <Cal calLink="mustaphachqoubi/let-s-talk" style={{width:"100%",height:"100%",overflow:"scroll"}} />;
+};
