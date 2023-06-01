272b32849d359c8f64db013421748125011456b1
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-06-01 07:30:04 +0100

    ok let's fix the clinet post request

--- backend/controllers/clientController.js
+++ backend/controllers/clientController.js
@@ -4,14 +4,11 @@ const Clients = require('../models/clientsModel.js')
 const createClient = async (req, res) => {
   const { firstName, lastName, optionSelected, optionId, service } = req.body
 
-    console.log(req.body)
   try{
     const client = await Clients.create({ firstName, lastName, optionSelected, optionId, service })
     res.status(200).json(client)
-    console.log(req.body)
   } catch (err) {
     res.status(404).json({ error: err.message})
-    console.log(req.body)
   }
 }
 
