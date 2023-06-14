05aeb2ed07fcfe78ae085b98b039ffd68b518154
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-06-14 11:56:35 +0100

    git keeps tracking node_modules

--- node_modules/.pnpm/@calcom+embed-core@1.2.1/node_modules/@calcom/embed-core/dist/src/sdk-event.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @file
- * This module is supposed to instantiate the SDK with appropriate namespace
- */
-import { SdkActionManager } from "./sdk-action-manager";
-export declare let sdkActionManager: SdkActionManager | null;
-//# sourceMappingURL=sdk-event.d.ts.map
